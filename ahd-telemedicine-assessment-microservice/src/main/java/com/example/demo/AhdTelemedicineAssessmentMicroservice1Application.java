98cac18335513ed6fa5acedbd6cde0230c94039b
accesshdigital <64785774+accesshdigital@users.noreply.github.com>
2021-02-22 16:12:53 +0530

    first comeet

---
+++ ahd-telemedicine-assessment-microservice/src/main/java/com/example/demo/AhdTelemedicineAssessmentMicroservice1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhdTelemedicineAssessmentMicroservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhdTelemedicineAssessmentMicroservice1Application.class, args);
+	}
+
+}
---
+++ ahd-telemedicine-assessment-microservice/src/test/java/com/example/demo/AhdTelemedicineAssessmentMicroservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhdTelemedicineAssessmentMicroservice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
