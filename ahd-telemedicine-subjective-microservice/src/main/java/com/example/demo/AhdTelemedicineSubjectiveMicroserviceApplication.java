6ae2a6ce8aa0e857a3854ed52d4c7adea5427c60
accesshdigital <64785774+accesshdigital@users.noreply.github.com>
2021-02-22 16:22:36 +0530

    first commit

---
+++ ahd-telemedicine-subjective-microservice/src/main/java/com/example/demo/AhdTelemedicineSubjectiveMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhdTelemedicineSubjectiveMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhdTelemedicineSubjectiveMicroserviceApplication.class, args);
+	}
+
+}
